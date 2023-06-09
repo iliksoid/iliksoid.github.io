07c251f0b07d77c0253653df55f58dc1a3d00e78
Ilia Agafonov <iliksoid@gmail.com>
2023-06-09 17:05:48 +0300

    Update JS to send data to the bot

--- app.js
+++ app.js
@@ -4,25 +4,67 @@ tg.expand();
 tg.MainButton.textColor = "#FFFFFF";
 tg.MainButton.color = "#c6a669";
 
-let item = "";
+//let query_id = tg.initDataUnsafe.query_id;
 
 let btn1 = document.getElementById("btn1");
 let btn2 = document.getElementById("btn2");
 let btn3 = document.getElementById("btn3");
 
-btn1.addEventListener("click", function() {
-    if (tg.MainButton.isVisible) {
+let orders = new Set();
+
+function toggleMainButton()
+{
+    if (orders.size == 0) {
         tg.MainButton.hide();
     }
     else {
-        tg.MainButton.setText("Вы выбрали товар 1");
-        item = "1";
+        tg.MainButton.setText("Выбрано товаров: " + orders.size);
         tg.MainButton.show();
     }
+}
+
+btn1.addEventListener("click", function() {
+    let product_id = "Argentina";
+    if (orders.has(product_id)) {
+        orders.delete(product_id);
+        btn1.firstChild.data = "Хочу"
+    }
+    else {
+        orders.add(product_id);
+        btn1.firstChild.data = "Не хочу"
+    }
+    toggleMainButton();
+});
+
+btn2.addEventListener("click", function() {
+    let product_id = "Colombo";
+    if (orders.has(product_id)) {
+        orders.delete(product_id);
+        btn2.firstChild.data = "Хочу"
+    }
+    else {
+        orders.add(product_id);
+        btn2.firstChild.data = "Не хочу"
+    }
+    toggleMainButton();
+});
+
+btn3.addEventListener("click", function() {
+    let product_id = "Peru";
+    if (orders.has(product_id)) {
+        orders.delete(product_id);
+        btn3.firstChild.data = "Хочу"
+    }
+    else {
+        orders.add(product_id);
+        btn3.firstChild.data = "Не хочу"
+    }
+    toggleMainButton();
 });
 
 Telegram.WebApp.onEvent("mainButtonClicked", function() {
-    tg.sendData(item);
+    web_app_data = Array.from(orders).join(", ");
+    tg.sendData(web_app_data);
 });
 
 
